0dad5eac4b899f2dd6ee5b05edcddbae9ca74323
Grupo1 <presentacion.entornos@gmail.com>
2019-01-28 20:51:51 +0100

    Juego Version 0.1

--- src/Pelota.java
+++ src/Pelota.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Pelota {
 
@@ -27,9 +28,9 @@ public class Pelota {
 		double dy = t * vy / 1000000000d;
 		x += dx;
 		y += dy;
-		if (x <= 0 || x + diametro >= dim.width)
+		if (x <= 100 || x + diametro+100 >= dim.width)   //cada x es para izq y pa abajo y la y pa derech ay arriba creo y eso es para el margen donde rebotan las pelotas
 			vx *= -1;
-		else if (y <= 0 || y + diametro >= dim.height)
+		else if (y <= 100 || y + diametro  + 100 >= dim.height)
 			vy *= -1;
 	}
 	
@@ -39,5 +40,7 @@ public class Pelota {
 		g.setColor(Color.BLACK);
 		g.drawOval((int) x, (int) y, diametro, diametro);
 	}
+	
+	
 	
 }
--- src/PelotasLocas.java
+++ src/PelotasLocas.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class PelotasLocas extends Juego {
 
-	private static final int MINRADIO = 50;
-	private static final int MAXRADIO = 200;
+	private static final int MINRADIO = 5;
+	private static final int MAXRADIO = 20;
 	private static final int MINVEL = 30;
 	private static final int MAXVEL = 200;
 	private static final Random r = new Random();
@@ -16,15 +16,17 @@ public class PelotasLocas extends Juego {
 		super(lienzo);
 		pelotas = new Pelota[numeroPelotas];
 		for (int i=0; i<pelotas.length; i++) {
-			Color color;
-			int radio;
-			double x;
-			double y;
-			double dir;
-			double vel;
+			Color color=new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256));
+			int radio=r.nextInt(MAXRADIO-MINRADIO+1)+MINRADIO;
+			double x = r.nextDouble()*601+100;		//por lo del main
+	        double y = r.nextDouble()*351+100;
+			double dir=r.nextDouble();
+			double vel= r.nextDouble()*(MAXVEL-MINVEL+1)+MINVEL;
 			pelotas[i] = new Pelota(color, radio, x, y, dir, vel, lienzo.getSize());
 		}
 	}
+
+	
 
 	@Override
 	public void siguiente(long ns) {
@@ -34,7 +36,6 @@ public class PelotasLocas extends Juego {
 
 	@Override
 	public void render(Graphics g) {
-		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, getLienzo().getWidth(), getLienzo().getHeight());
 		for (int i=0; i<pelotas.length; i++)
 			pelotas[i].paint(g);
